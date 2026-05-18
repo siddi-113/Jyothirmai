d58906ceea2f23a5d57526341098874ca2cbbf40
Satya Kola <146601861+satyasrinivaskola@users.noreply.github.com>
2026-05-18 12:23:45 +0530

    Fix variable naming and update logic in taskjs3.js
    Updated file

--- taskjs3.js
+++ taskjs3.js
@@ -2,7 +2,7 @@ console.log("hello");
 let count;
 let jyo;
 let isrunning=false
-let update_count;
+let updated_count;
 let a=document.createElement("p");
 function submit_one(){
     
@@ -20,45 +20,45 @@ else {
     count=last;
 }
   
-     if(isrunning=true){
+
+function data(){
+   if(isrunning===true){
         
    
-        count=update_count;
-        
+        count=updated_count;
+           isrunning=false; 
          console.log("Hello count"+count )
           console.log("Hello update_count"+update_count )
-          
+      
     } 
-function data(){
-  
     count--
     console.log(count);
-   
   
-     
  a.textContent=count;
     b.appendChild(a);
      
      if(count===0){
          a.textContent="time exeed";
          a.style.color="orange";
+        //   updated_count=count;
         clearInterval(jyo);
+         isrunning=false;
     }
      
-    update_count=count
+
    
 }
 
 jyo=setInterval(data,1000);
- isrunning=true;
+
 }
-console.log("running"+isrunning)
+
 function submit_two(){
     clearInterval(jyo);
-    isrunning=false;
+     updated_count=count;
+    isrunning=true;
 a.textContent=count+ "you stopped this";
 a.style.color="red";
 
 }
-
 
